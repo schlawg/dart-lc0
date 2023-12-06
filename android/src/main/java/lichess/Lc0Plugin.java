e608877fb8ba68025f6eaa3b3053688445b100df
Jonathan Gamble <gamblej@gmail.com>
2023-12-06 17:35:01 -0600

    gah

---
+++ android/src/main/java/lichess/Lc0Plugin.java
@@ -0,0 +1,17 @@
+package com.lc0;
+
+import androidx.annotation.NonNull;
+
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+
+public class Lc0Plugin implements FlutterPlugin {
+  @Override
+  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+    // intentionally left empty
+  }
+
+  @Override
+  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+    // intentionally left empty
+  }
+}
